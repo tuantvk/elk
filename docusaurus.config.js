2b65f370b8312df04b6a30991de5c8463fccc636
tuantv <tuanhanhphuc123@gmail.com>
2020-10-16 10:29:01 +0700

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: 'Elk',
+  tagline: 'Documents for React Native testing',
+  url: '/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'tuantvk',
+  projectName: 'elk',
+  themeConfig: {
+    navbar: {
+      title: 'Elk',
+      logo: {
+        alt: 'Elk Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/tuantvk/elk',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} tuantvk, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
